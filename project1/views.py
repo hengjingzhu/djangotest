578c9c90b5074f69693268677f50538072523795
davidzhu <david3311041@hotmail.com>
2022-03-02 04:19:52 +0800

    0.1.9

--- project1/views.py
+++ project1/views.py
@@ -2,8 +2,4 @@ from django.http import HttpResponse
 
 
 def testview(request):
-<<<<<<< HEAD
-    return HttpResponse('FIRSTgit fad123 ，versions 0.1.8')
-=======
     return HttpResponse('FIRST DJANGO VIEW，version 0.1.2')
->>>>>>> b947c8e51d94aef24e05d453efb4e12719d81618
